5da7241807352439b41f24775ee135cfffc1d1fe
Ariel Veras <ariolvs123@gmail.com>
2022-06-28 13:27:03 -0400

    [tarefa] Adicionando cors [new] adicionado o cors para aceitar as requisoções que vim do react.

--- AppSquadra/Startup.cs
+++ AppSquadra/Startup.cs
@@ -33,6 +33,13 @@ namespace AppSquadra
                 app.UseSwaggerUI();
             }
 
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:3000");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
